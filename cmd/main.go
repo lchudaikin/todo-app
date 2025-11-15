68d7bf14ba205b97e73f719e46096d5b33af3390
Lev <lev.chudaikin@gmail.com>
2025-11-15 19:53:54 +0200

    implemented application architecture and DB connection from app

--- cmd/main.go
+++ cmd/main.go
@@ -1,20 +1,39 @@
 package main
 
 import (
-	"log"
-
+	"github.com/joho/godotenv"
 	"github.com/lchudaikin/todo-app"
 	"github.com/lchudaikin/todo-app/pkg/handler"
 	"github.com/lchudaikin/todo-app/pkg/repository"
 	"github.com/lchudaikin/todo-app/pkg/service"
+	_ "github.com/lib/pq"
 	"github.com/spf13/viper"
+	"log"
+	"os"
 )
 
 func main() {
 	if err := initConfig(); err != nil {
 		log.Fatalf("init config error: %s", err.Error())
 	}
-	repos := repository.NewRepository()
+
+	if err := godotenv.Load(); err != nil {
+		log.Fatalf("Error loading .env file: %s", err.Error())
+	}
+
+	db, err := repository.NewPostgresDB(repository.Config{
+		Host:     viper.GetString("db.host"),
+		Port:     viper.GetString("db.port"),
+		Username: viper.GetString("db.username"),
+		DBName:   viper.GetString("db.dbname"),
+		SSLMode:  viper.GetString("db.sslmode"),
+		Password: os.Getenv("DB_PASSWORD"),
+	})
+	if err != nil {
+		log.Fatalf("init db error: %s", err.Error())
+	}
+
+	repos := repository.NewRepository(db)
 	services := service.NewService(repos)
 	handlers := handler.NewHandler(services)
 
