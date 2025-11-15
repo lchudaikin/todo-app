e7d4337f84e57630e8f4b644af917d79d3b49feb
Lev <lev.chudaikin@gmail.com>
2025-11-15 15:21:39 +0200

    added database

--- cmd/main.go
+++ cmd/main.go
@@ -5,12 +5,27 @@ import (
 
 	"github.com/lchudaikin/todo-app"
 	"github.com/lchudaikin/todo-app/pkg/handler"
+	"github.com/lchudaikin/todo-app/pkg/repository"
+	"github.com/lchudaikin/todo-app/pkg/service"
+	"github.com/spf13/viper"
 )
 
 func main() {
-	handlers := new(handler.Handler)
+	if err := initConfig(); err != nil {
+		log.Fatalf("init config error: %s", err.Error())
+	}
+	repos := repository.NewRepository()
+	services := service.NewService(repos)
+	handlers := handler.NewHandler(services)
+
 	srv := new(todo.Server)
-	if err := srv.Run("8080", handlers.InitRoutes()); err != nil {
+	if err := srv.Run(viper.GetString("8000"), handlers.InitRoutes()); err != nil {
 		log.Fatalf("error occured while running http server: %s", err.Error())
 	}
+}
+
+func initConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
 }
